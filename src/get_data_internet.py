7d82759558dfd6269a9cb23a638a774ea7da6755
Brenno-Lualdi <brennolualdi@yahoo.com.br>
2025-05-04 11:41:00 -0300

    chore: implement generic get_values from internet

--- src/get_data_internet.py
+++ src/get_data_internet.py
@@ -29,10 +29,10 @@ class BasicData:
 
     def get_info_wikipedia(self):
         try:
-            text = wikipedia.summary("Empresa: " + info_action["nome"], 3)
+            text = wikipedia.summary(info_action["nome"], 3)
             return GoogleTranslator(source="auto", target="pt").translate(text)
         except:
-            log.error("Não conseguiu pegar da Wikipédia/traduzir")
+            log.error(f'Não conseguiu pegar informação da {info_action["nome"]} na Wikipédia/traduzir')
             return "Nada encontrado...."
 
     def get_datas_internet(self):
@@ -66,13 +66,6 @@ class BasicData:
 # ------------------------------------------
 
 def get_values_money_bdrs(soup):
-    info_action["preco_min_cota"] = soup.find_all("strong")[1].text
-    info_action["preco_max_cota"] = soup.find_all("strong")[2].text
-    info_action["ultimo_pagamento"] = soup.find_all("span")[33].text
-    info_action["valor_cota"] = soup.find('strong').text
-    info_action["dy"] = soup.find_all("strong")[3].text
-    info_action["valorizacao_cota"] = soup.find_all("strong")[4].text
-    info_action["oscilacao_cota"] = soup.find_all('b')[11].text.strip("\n").lstrip().rstrip()
     info_action["cnpj"] = "none"
     info_action["linkSiteRi"] = "none"
 
@@ -82,11 +75,7 @@ def get_values_money_bdrs(soup):
 # ----------------------------------------
 
 def get_values_money_etfs(soup):
-    info_action["valor_cota"] = soup.find('strong').text
     info_action["dy"] = None
-    info_action["oscilacao_cota"] = soup.find_all('b')[11].text.strip("\n").lstrip().rstrip()
-    info_action["preco_min_cota"] = soup.find_all("strong")[1].text
-    info_action["preco_max_cota"] = soup.find_all("strong")[2].text
     info_action["ultimo_pagamento"] = None
     info_action["cnpj"] = "none"
     info_action["linkSiteRi"] = "none"
@@ -96,41 +85,54 @@ def get_values_money_etfs(soup):
 
 # -----------------------
 
-def get_values_money_stocks(soup):
+def get_generic_money_values(soup):
     try:
         info_action["dy"] = soup.find_all("div", {'title': 'Dividend Yield com base nos últimos 12 meses'})[0].find(
             'strong').text
     except:
         info_action["dy"] = 0
-    info_action["preco_min_cota"] = soup.find_all("div", {'title': 'Valor mínimo das últimas 52 semanas'})[0].find(
-        'strong').text
-    info_action["preco_max_cota"] = soup.find_all("div", {'title': 'Valor máximo das últimas 52 semanas'})[0].find(
-        'strong').text
+    try:
+        info_action["preco_min_cota"] = soup.find_all("div", {'title': 'Valor mínimo das últimas 52 semanas'})[0].find(
+            'strong').text
+    except:
+        info_action["preco_min_cota"] = 0
+    try:
+        info_action["preco_max_cota"] = soup.find_all("div", {'title': 'Valor máximo das últimas 52 semanas'})[0].find(
+            'strong').text
+    except:
+        info_action["preco_max_cota"] = 0
 
-    info_action["ultimo_pagamento"] = (soup.find('div', {"id": "earning-section"})
-                                       .select("div > div > div:nth-child(2) > table > tbody > tr:nth-child(1) > "
-                                               "td:nth-child(3)"))[0].text
-    info_action["valor_cota"] = soup.find_all("div", {'title': 'Valor atual do ativo'})[0].find('strong').text
-    info_action["oscilacao_cota"] = \
-        soup.find_all("span", {'title': 'Variação do valor do ativo com base no dia anterior'})[0].find('b').text
+    try:
+        info_action["ultimo_pagamento"] = (soup.find('div', {"id": "earning-section"})
+                                           .select("div > div > div:nth-child(2) > table > tbody > tr:nth-child(1) > "
+                                                   "td:nth-child(3)"))[0].text
+    except:
+        info_action["ultimo_pagamento"] = 0
+
+    try:
+        info_action["valor_cota"] = soup.find_all("div", {'title': 'Valor atual do ativo'})[0].find('strong').text
+    except:
+        info_action["valor_cota"] = 0
+    try:
+        oscilation = soup.find_all("div", {'title': 'Variação do valor do ativo com base no dia anterior'})[0]
+        signal = '+' if 'upward' in oscilation.find('i').text else '-'
+        info_action["oscilacao_cota"] = signal + oscilation.find('b').text
+    except:
+        info_action["oscilacao_cota"] = 0
     try:
         info_action["valorizacao_cota"] = soup.find_all("div", {'title': 'Valorização no preço do ativo com base nos '
                                                                          'últimos 12 meses'})[0].find('strong').text
     except:
-        log.error(soup.find_all("strong", attrs={"class": "value"}))
         info_action["valorizacao_cota"] = 0.0
     try:
         info_action["cnpj"] = soup.find_all("small", attrs={"class": "d-block fs-4 fw-100 lh-4"})[0].text
     except:
-        log.error(soup.find_all("small", attrs={"class": "d-block fs-4 fw-100 lh-4"}))
         info_action["cnpj"] = None
     try:
         info_action["linkSiteRi"] = soup.find_all("a", attrs={"rel": "noopener noreferrer nofollow",
                                                               "class": "waves-effect waves-light btn btn-small "
                                                                        "btn-secondary"})[0]["href"]
     except:
-        log.error(soup.find_all("a", attrs={"rel": "noopener noreferrer nofollow",
-                                            "class": "waves-effect waves-light btn btn-small btn-secondary"}))
         info_action["linkSiteRi"] = None
 
     return info_action
@@ -139,13 +141,7 @@ def get_values_money_stocks(soup):
 # -----------------------
 
 def get_values_money_fiis(soup):
-    info_action["preco_min_cota"] = soup.find_all("strong")[1].text
-    info_action["preco_max_cota"] = soup.find_all("strong")[2].text
-    info_action["ultimo_pagamento"] = soup.find_all("span")[33].text
-    info_action["valor_cota"] = soup.find('strong').text
-    info_action["dy"] = soup.find_all("strong")[3].text
-    info_action["oscilacao_cota"] = soup.find_all('b')[11].text.strip("\n").lstrip().rstrip()
-    info_action["valorizacao_cota"] = soup.find_all("strong", attrs={"class": "value"})[4].text
+
     info_action["cnpj"] = soup.find_all("strong", attrs={"class": "value"})[28].text
     info_action["linkSiteRi"] = "none"
 
@@ -156,16 +152,18 @@ def get_values_money_fiis(soup):
 
 def get_all_values_from_nav(soup, ticker, stock_type):
     comand_basics = BasicData(soup, ticker)
+    info_action = {}  # Initialize info_action as an empty dictionary
     comand_basics.get_datas_internet()
+    info_action = get_generic_money_values(soup)
 
     if 'acoes' in stock_type:
-        info_action = get_values_money_stocks(soup)
+        pass
     elif 'fundos-imobiliarios' in stock_type:
-        info_action = get_values_money_fiis(soup)
+        get_values_money_fiis(soup)
     elif 'etfs' in stock_type:
-        info_action = get_values_money_etfs(soup)
+        get_values_money_etfs(soup)
     else:
-        info_action = get_values_money_bdrs(soup)
+        get_values_money_bdrs(soup)
 
     comand_basics.prepare_data()
     DataLocal().write_data(info_action)
