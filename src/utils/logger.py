dd9a9c56f9acedb867cafa12d427dd890259a0d0
Brenno-Lualdi <brennolualdi@yahoo.com.br>
2025-05-04 08:58:34 -0300

    chore: set colors to log by loglevel

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,14 +1,27 @@
 import logging
 from datetime import datetime
+from colorama import Fore, Style, init
+
+init(autoreset=True)
 
 
 class CustomFormatter(logging.Formatter):
+    LEVEL_COLORS = {
+        "DEBUG": Fore.BLUE,
+        "INFO": Fore.GREEN,
+        "WARNING": Fore.YELLOW,
+        "ERROR": Fore.RED,
+        "CRITICAL": Fore.MAGENTA,
+    }
+
     def formatTime(self, record, datefmt=None):
         dt = datetime.fromtimestamp(record.created)
         return dt.strftime("%Y/%m/%d %H:%M:%S:%f")[:-3]
 
     def format(self, record):
         record.class_method = f"{record.module}.{record.funcName}"
+        log_color = self.LEVEL_COLORS.get(record.levelname, "")
+        record.levelname = f"{log_color}{record.levelname}"
         return super().format(record)
 
 
