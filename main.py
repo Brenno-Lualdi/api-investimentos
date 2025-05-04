f366dd877ebf6cfb33d049976d894c2ff186b698
Brenno-Lualdi <brennolualdi@yahoo.com.br>
2025-05-04 11:39:25 -0300

    refactor: implements python naming conventions

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ info_action = {"data": {}}
 
 @app.get("/get-ticker/{ticker}")
 async def getTicker(ticker: str):
-    dataLocal = DataLocal()
-    info_action["data"] = dataLocal.get_data(ticker)
+    data_local = DataLocal()
+    info_action["data"] = data_local.get_data(ticker)
     return info_action
 
 
 @app.get("/get-tickers")
 async def getTickers():
-    dataLocal = DataLocal()
-    return dataLocal.get_order_datas("oscilacaoCota")
+    data_local = DataLocal()
+    return data_local.get_order_datas("oscilacaoCota")
 
 
 @app.get("/get-stocks-by-order/{order}")
 async def getStocksByOrder(order: str):
-    dataLocal = DataLocal()
-    return dataLocal.get_order_datas(order)
+    data_local = DataLocal()
+    return data_local.get_order_datas(order)
 
 
 uvicorn.run(app, host="localhost", port=8000)
--- src/update_values.py
+++ src/update_values.py
@@ -5,14 +5,14 @@ from bs4 import BeautifulSoup
 from get_data_internet import get_all_values_from_nav
 from src.utils.logger import log
 
-totalLengthStocks = len(stocks) + len(fiis) + len(bdrs) + len(etfs)
+total_length_stocks = len(stocks) + len(fiis) + len(bdrs) + len(etfs)
 total_stocks = stocks
 total_stocks = numpy.append(total_stocks, fiis)
 total_stocks = numpy.append(total_stocks, bdrs)
 total_stocks = numpy.append(total_stocks, etfs)
 
 log.debug("Rodando dnv")
-for x in range(1, totalLengthStocks):
+for x in range(1, total_length_stocks):
     ticker = total_stocks[x]
 
     if ticker in stocks:
@@ -26,5 +26,8 @@ for x in range(1, totalLengthStocks):
 
     url = f"https://statusinvest.com.br/{stock_type}/{ticker}"
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    if response.status_code != 200:
+        log.error(f"Error fetching data for {ticker}: {response.status_code}")
+        continue
     nav = BeautifulSoup(response.text, "html5lib")
-    infoAction = get_all_values_from_nav(nav, ticker.upper(), stock_type)
+    get_all_values_from_nav(nav, ticker.upper(), stock_type)
